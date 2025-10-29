71a51dfaee8557c52f82f131132286facca2476c
Staniswow <stan@ng-sensors.com>
2025-10-29 22:56:30 +0100

    Changed ForceProperty startup settings

--- Assets/Scripts/ForceProperty.cs
+++ Assets/Scripts/ForceProperty.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.UIElements;
 using UnityEngine;
+using static ForceManager;
 
 public class ForceProperty : MonoBehaviour
 {
-    [SerializeField] public ForceManager.ForceType forceType;
+    [SerializeField] public ForceType forceType = ForceType.Gravity;
     [SerializeField] public bool forceApplier = true;
     [SerializeField] public bool forceReceiver = true;
-    [SerializeField] public float maxForceValue = 100;
-    [SerializeField] [Tooltip("Force will scale between 0 and forceMaxRange")] public float maxForceApplyRange;
-    [SerializeField] public ForceManager.ForceFalloffType forceFalloffType;
+    [SerializeField] public float maxForceValue = 25;
+    [SerializeField] [Tooltip("Force will scale between 0 and forceMaxRange")] public float maxForceApplyRange = 10;
+    [SerializeField] public ForceManager.ForceFalloffType forceFalloffType = ForceFalloffType.Quadratic;
     [SerializeField] [Tooltip("Only used if ForceFallofType is CustomCurve")] public AnimationCurve forceFalloffCurve;
 
     private void OnEnable()
