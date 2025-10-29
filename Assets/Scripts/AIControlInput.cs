287c3a6ddd2afaba3096cb39827e00e4a9fdb598
Staniswow <stan@ng-sensors.com>
2025-10-29 15:56:42 +0100

    Initialized the file that will control the ship later

---
+++ Assets/Scripts/AIControlInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIControlInput : ShipControlInput
+{
+    public override float GetSteerInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override float GetThrustInput()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
